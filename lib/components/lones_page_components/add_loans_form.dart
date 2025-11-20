d36333551027ef67523974154d1aea31d5118e09
irfanekon <irfanekon2004@gmail.com>
2025-11-20 21:25:54 +0600

    added loan issue form on loans page

---
+++ lib/components/lones_page_components/add_loans_form.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+class AddLoansForm extends StatelessWidget {
+  final void Function()? onCancel;
+  const AddLoansForm({super.key, required this.onCancel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: .start,
+        children: [
+          Row(
+            mainAxisAlignment: .spaceBetween,
+            children: [
+              Text(
+                "Issue New Loan",
+                style: TextStyle(fontSize: 18, fontWeight: .bold),
+              ),
+
+              IconButton(
+                onPressed: onCancel,
+                icon: Icon(Icons.close, color: Colors.grey.shade800),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Select Member',
+            style: TextStyle(fontSize: 16, fontWeight: .w600),
+          ),
+          SizedBox(height: 8),
+          TextField(
+            cursorColor: Colors.grey.shade800,
+            decoration: InputDecoration(
+              isDense: true,
+              filled: true,
+              fillColor: Colors.grey.shade100,
+              hintText: 'Select member...',
+              enabledBorder: OutlineInputBorder(
+                borderRadius: .circular(12),
+                borderSide: BorderSide(color: Colors.grey.shade300),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(width: 2, color: Colors.green.shade800),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Loan Amount (৳)',
+            style: TextStyle(fontSize: 16, fontWeight: .w600),
+          ),
+          SizedBox(height: 8),
+          TextField(
+            cursorColor: Colors.grey.shade800,
+            decoration: InputDecoration(
+              filled: true,
+              isDense: true,
+              fillColor: Colors.grey.shade100,
+              hintText: 'XXXXX',
+              enabledBorder: OutlineInputBorder(
+                borderRadius: .circular(12),
+                borderSide: BorderSide(color: Colors.grey.shade300),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(width: 2, color: Colors.green.shade800),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      'Interest Rate (%)',
+                      style: TextStyle(fontSize: 16, fontWeight: .w600),
+                    ),
+                    SizedBox(height: 8),
+                    TextField(
+                      cursorColor: Colors.grey.shade800,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.grey.shade100,
+                        hintText: 'XXX.XX',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: .circular(12),
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.green.shade800,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      'Duration (Months)',
+                      style: TextStyle(fontSize: 16, fontWeight: .w600),
+                    ),
+                    SizedBox(height: 8),
+                    TextField(
+                      cursorColor: Colors.grey.shade800,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.grey.shade100,
+                        hintText: 'Select duration',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.green.shade800,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: onCancel,
+                  child: Container(
+                    margin: EdgeInsets.only(right: 5),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: .bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(right: 5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.green.shade800,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey.shade300),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Issue Loan',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: .bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/loans_page.dart
+++ lib/pages/loans_page.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:microflow/components/lones_page_components/add_loans_form.dart';
 
-class LoansPage extends StatelessWidget {
+class LoansPage extends StatefulWidget {
   const LoansPage({super.key});
 
   @override
+  State<LoansPage> createState() => _LoansPageState();
+}
+
+class _LoansPageState extends State<LoansPage> {
+  bool showForm = false;
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Loans"));
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Loans",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Total Issued: ৳ 200,000",
+                        style: TextStyle(
+                          color: Color(0xFF929292),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showForm = !showForm;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.green.shade800,
+                      ),
+                      child: Icon(
+                        showForm ? Icons.add : Icons.remove,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                child: showForm
+                    ? null
+                    : AddLoansForm(
+                        onCancel: () {
+                          setState(() {
+                            showForm = true;
+                          });
+                        },
+                      ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
